d60aec22a794f6b7b4c0270b33da8d54b3aa1155
Christina Chen <cyc25>
2018-10-23 15:52:01 -0400

    finished inputting question functionality and authentication into firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import { HttpClientModule } from '@angular/common/http';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { CommitteeLoginPage } from '../pages/committee-login/committee-login';
 import { QuestionsPage } from '../pages/questions/questions';
 import { SubmitsuccessPage } from '../pages/submitsuccess/submitsuccess';
 import { CommitteeOptionsPage } from '../pages/committee-options/committee-options';
+import { QuestionsserviceProvider } from '../providers/questionsservice/questionsservice';
+import { UsersServiceProvider } from '../providers/users-service/users-service';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireModule } from '@angular/fire';
 
+
+const config = {
+  apiKey: "AIzaSyCTFrdcLsW5gtU7QohQ68M82ijy-NTAG7o",
+  authDomain: "trivia-site.firebaseapp.com",
+  databaseURL: "https://trivia-site.firebaseio.com",
+  projectId: "trivia-site",
+  storageBucket: "trivia-site.appspot.com",
+  messagingSenderId: "344315610954"
+};
 @NgModule({
   declarations: [
     MyApp,
@@ -22,7 +36,10 @@ import { CommitteeOptionsPage } from '../pages/committee-options/committee-optio
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    HttpClientModule,
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(config),
+    AngularFireDatabaseModule,        
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -36,6 +53,9 @@ import { CommitteeOptionsPage } from '../pages/committee-options/committee-optio
   providers: [
     StatusBar,
     SplashScreen,
+    AngularFireAuth,
+    QuestionsserviceProvider,
+    UsersServiceProvider,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/committee-login/committee-login.ts
+++ src/pages/committee-login/committee-login.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController, ToastController } from 'ionic-angular';
 import { QuestionsPage } from '../questions/questions';
 import { CommitteeOptionsPage } from '../committee-options/committee-options';
+import { UsersServiceProvider } from '../../providers/users-service/users-service';
 
 /**
  * Generated class for the CommitteeLoginPage page.
@@ -16,8 +17,10 @@ import { CommitteeOptionsPage } from '../committee-options/committee-options';
   templateUrl: 'committee-login.html',
 })
 export class CommitteeLoginPage {
+  public email: string;
+  public password: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public usersService: UsersServiceProvider, public toastCtrl: ToastController, public loadingCtrl: LoadingController, public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
@@ -25,7 +28,23 @@ export class CommitteeLoginPage {
   }
 
   goToOptions() {
-    this.navCtrl.push(CommitteeOptionsPage);
+    var loader = this.loadingCtrl.create({
+      content: "Please wait..."
+    });
+    loader.present();
+
+    this.usersService.loginUser(this.email,this.password).then(authData => {
+      loader.dismiss();
+      this.navCtrl.push(CommitteeOptionsPage);
+    }, error => {
+      loader.dismiss();
+      let toast = this.toastCtrl.create({
+        message: error,
+        duration: 3000,
+        position: 'top'
+      });
+    })
+
   }
 
 
